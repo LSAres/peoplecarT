fd12572db42c488b5f27be3c92d605fb369eb1df
lin <1360184954@qq.com>
2017-09-21 16:03:42 +0800

    充值代码添加

--- game/Mobile/Controller/FinancemanagementController.class.php
+++ game/Mobile/Controller/FinancemanagementController.class.php
@@ -5,6 +5,9 @@ class FinancemanagementController extends CommonController {
     public function finance_bonusdetailed(){
         $this->display();
     }
+    public function finance_charge(){
+        $this->display();
+    }
 
     public function finance_detailed(){
         $this->display();
@@ -19,5 +22,31 @@ class FinancemanagementController extends CommonController {
     }
     public function finance_withdrawals(){
         $this->display();
+    }
+
+    public function addcharge(){
+        $t=I('post.');
+        foreach($t as $v){
+            if($v == ''){
+                echo "<script>alert('请确认输入完成');</script>";
+                echo "<script>javascript:history.back(-1);</script>";die;
+            }
+        }
+        $userid = session('userid');
+        $money = I('post.money');
+        $phone = I('post.phone');
+        $wx_no = I('post.wx_no');
+        if($money<=0){
+            echo "<script>alert('金额数值错误');</script>";
+            echo "<script>javascript:history.back(-1);</script>";die;
+        }
+        $data['uid']=$userid;
+        $data['money'] = $money;
+        $data['phone'] = $phone;
+        $data['wx_no'] = $wx_no;
+        $data['time'] = time();
+        $data['status'] = 0;
+        $res = M('charge')->data($data)->add();
+
     }
 }
