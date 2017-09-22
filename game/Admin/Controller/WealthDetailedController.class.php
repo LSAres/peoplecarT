2b41da4d3cd49d653bdfa1a2eb80e415196f7786
898254495 <q898254495@hotmail.com>
2017-09-22 16:59:02 +0800

    添加后台提现与充值记录代码

--- game/Admin/Controller/WealthDetailedController.class.php
+++ game/Admin/Controller/WealthDetailedController.class.php
@@ -7,6 +7,27 @@ class WealthDetailedController extends CommonController{
         $this->display();
     }
     public function cashHistory(){
+        $where = null;
+        $tb_new = M('withdraw');
+        $pagesize =10;
+        $p = getpage($tb_new, $where, $pagesize);
+        $pageshow   = $p->show();
+
+        $newArr=$tb_new->where($where)
+            ->field('id,uid,realname,bank_name,bank_address,bank_card,money,status,time')
+            ->order('id desc ')
+            ->select();
+
+        foreach($newArr as $k=>$v){
+            $userInfo = M('user')->where('userid='.$v['uid'])->find();
+            $newArr[$k]['account'] = $userInfo['account'];
+            $newArr[$k]['username'] = $userInfo['username'];
+        }
+
+        $this->assign(array(
+            'userArr'=>$newArr,
+            'pageshow'=>$pageshow,
+        ));
         $this->display();
     }
     public function refundAplyHistory(){
@@ -19,6 +40,27 @@ class WealthDetailedController extends CommonController{
         $this->display();
     }
     public function userChargeHistory(){
+        $where = null;
+        $tb_new = M('charge');
+        $pagesize =10;
+        $p = getpage($tb_new, $where, $pagesize);
+        $pageshow   = $p->show();
+
+        $newArr=$tb_new->where($where)
+            ->field('id,uid,phone,wx_no,money,time,status')
+            ->order('id desc ')
+            ->select();
+
+        foreach($newArr as $k=>$v){
+            $userInfo = M('user')->where('userid='.$v['uid'])->find();
+            $newArr[$k]['account'] = $userInfo['account'];
+            $newArr[$k]['username'] = $userInfo['username'];
+        }
+
+        $this->assign(array(
+            'userArr'=>$newArr,
+            'pageshow'=>$pageshow,
+        ));
         $this->display();
     }
     public function userDetails(){
