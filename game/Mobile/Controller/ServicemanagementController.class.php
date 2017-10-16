587bdefdf786233f23cda18b6da14b3340d917b4
1360184954 <1360184954@qq.com>
2017-10-16 17:27:09 +0800

    会员身份升级代码添加

--- game/Mobile/Controller/ServicemanagementController.class.php
+++ game/Mobile/Controller/ServicemanagementController.class.php
@@ -3,7 +3,33 @@ namespace Mobile\Controller;
 use Think\Controller;
 class ServicemanagementController extends CommonController {
     public function user_memberupgrade(){
-        $this->display();
+        if(!I('post.')) {
+            $userid = session('userid');
+            $userInfo = M('user')->where('userid=' . $userid)->find();
+            if ($userInfo['leve'] == 1) {
+                $userInfo['shenfen'] = "普通会员";
+            }
+            if ($userInfo['leve'] == 2) {
+                $userInfo['shenfen'] = "代理商";
+            }
+            $this->assign('userInfo', $userInfo);
+            $this->display();
+        }else{
+            $degree=I('post.degree');
+            $userid=session('userid');
+            $data['uid']=$userid;
+            $data['degree']=$degree;
+            $data['status']=0;
+            $data['time']=time();
+            $res = M('change_degree')->data($data)->add();
+            if($res){
+                echo "<script>alert('申请成功，请等待客服人员与您联系');location.href='".U('Index/copyPageTwo')."'</script>";
+                exit();
+            }else{
+                echo "<script>alert('申请失败');</script>";
+                echo "<script>javascript:history.back(-1);</script>";die;
+            }
+        }
     }
     public function user_register(){
         $this->display();
