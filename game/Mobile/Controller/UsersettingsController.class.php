bfbdc3ddf2431db9edd5e40b6ccc63b114358dca
1360184954 <1360184954@qq.com>
2017-10-16 09:38:17 +0800

    用户页面结构图代码添加

--- game/Mobile/Controller/UsersettingsController.class.php
+++ game/Mobile/Controller/UsersettingsController.class.php
@@ -15,6 +15,9 @@ class UsersettingsController extends CommonController {
     }
 
     public function user_privateemail(){
+        $userid = session('userid');
+        $private_email = M('useremail')->where('uid='.$userid)->select();
+        $this->assign('private_email',$private_email);
         $this->display();
     }
 
@@ -23,6 +26,9 @@ class UsersettingsController extends CommonController {
     }
 
     public function user_systemnotice(){
+        $db_new = M('new');
+        $new_list = $db_new->where()->select();
+        $this->assign('new_list',$new_list);
         $this->display();
     }
 
