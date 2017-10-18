979b87b64533b1e7613c92bf7e3913eae671169b
1360184954 <1360184954@qq.com>
2017-10-18 16:21:43 +0800

    打款记录代码添加

--- game/Admin/Controller/WealthDetailedController.class.php
+++ game/Admin/Controller/WealthDetailedController.class.php
@@ -34,6 +34,21 @@ class WealthDetailedController extends CommonController{
         $this->display();
     }
     public function remittanceHistory(){
+        $where = null;
+        $tb_new = M('remittance');
+        $pagesize =10;
+        $p = getpage($tb_new, $where, $pagesize);
+        $pageshow   = $p->show();
+
+        $newArr=$tb_new->where($where)
+            ->field('id,receivables_account,receivables_username,remittance_money,remittance_bank,remittance_user')
+            ->order('id desc ')
+            ->select();
+
+        $this->assign(array(
+            'userArr'=>$newArr,
+            'pageshow'=>$pageshow,
+        ));
         $this->display();
     }
     public function userCapitalOffset(){
@@ -65,5 +80,38 @@ class WealthDetailedController extends CommonController{
     }
     public function userDetails(){
         $this->display();
+    }
+
+    public function appendNewMessageButton(){
+        if(!I('post.')){
+            $this->display();
+        }else{
+            $t=I('post.');
+            foreach($t as $v){
+                if($v == ''){
+                    echo "<script>alert('请确认输入完成');</script>";
+                    echo "<script>javascript:history.back(-1);</script>";die;
+                }
+            }
+            $receivables_account=I('post.receivables_account');
+            $receivables_username=I('post.receivables_username');
+            $remittance_money=I('post.remittance_money');
+            $remittance_bank=I('post.remittance_bank');
+            $remittance_user=I('post.remittance_user');
+            $data['receivables_account']=$receivables_account;
+            $data['receivables_username']=$receivables_username;
+            $data['remittance_money']=$remittance_money;
+            $data['remittance_bank']=$remittance_bank;
+            $data['remittance_user']=$remittance_user;
+            $data['time']=time();
+            $res = M('remittance')->data($data)->add();
+            if($res){
+                echo "<script>alert('添加成功');location.href='".U('WealthDetailed/remittanceHistory')."'</script>";
+                exit();
+            }else{
+                echo "<script>alert('添加失败');</script>";
+                echo "<script>javascript:history.back(-1);</script>";die;
+            }
+        }
     }
 }
