e114f3c7c7fc55c7a64b3c212213076a54b45d7d
1360184954 <1360184954@qq.com>
2017-10-19 14:52:49 +0800

    代理代码添加

--- game/Mobile/Controller/ServicemanagementController.class.php
+++ game/Mobile/Controller/ServicemanagementController.class.php
@@ -49,7 +49,123 @@ class ServicemanagementController extends CommonController {
         if(!I('post.')){
             $this->display();
         }else{
+            $province = I('post.province');
+            $provinceDataCode = I('post.provinceDataCode');
+            if(!$province){
+                echo "<script>alert('请选择要代理的省份');</script>";
+                echo "<script>javascript:history.back(-1);</script>";die;
+            }
+            if(!$provinceDataCode){
+                echo "<script>alert('系统出错，请重新选择要代理的省份');</script>";
+                echo "<script>javascript:history.back(-1);</script>";die;
+            }
+            $userid = session('userid');
+            $data['uid']=$userid;
+            $data['type']=1;
+            $data['province']=$province;
+            $data['province_code']=$provinceDataCode;
+            $date['time']=time();
+            $data['status']=0;
+            $res = M('apply_agent')->data($data)->add();
+            if($res){
+                echo "<script>alert('申请成功，请等待客服人员与您联系');location.href='".U('Index/copyPageTwo')."'</script>";
+                exit();
+            }else{
+                echo "<script>alert('申请失败');</script>";
+                echo "<script>javascript:history.back(-1);</script>";die;
+            }
+        }
+    }
 
+    public function user_cityapply(){
+        if(!I('post.')){
+            $this->display();
+        }else{
+            $province = I('post.province');
+            $provinceDataCode = I('post.provinceDataCode');
+            $city = I('post.city');
+            $cityDataCode = I('post.cityDataCode');
+
+            if(!$province){
+                echo "<script>alert('请选择要代理的省份');</script>";
+                echo "<script>javascript:history.back(-1);</script>";die;
+            }
+            if(!$provinceDataCode){
+                echo "<script>alert('系统出错，请重新选择要代理的省份');</script>";
+                echo "<script>javascript:history.back(-1);</script>";die;
+            }
+            if(!$city){
+                echo "<script>alert('请选择要代理的城市');</script>";
+                echo "<script>javascript:history.back(-1);</script>";die;
+            }
+            if(!$cityDataCode){
+                echo "<script>alert('系统出错，请重新选择要代理的城市');</script>";
+                echo "<script>javascript:history.back(-1);</script>";die;
+            }
+            $userid = session('userid');
+            $data['uid']=$userid;
+            $data['type']=2;
+            $data['province']=$province;
+            $data['province_code']=$provinceDataCode;
+            $data['city']=$city;
+            $data['city_code']=$cityDataCode;
+            $date['time']=time();
+            $data['status']=0;
+            $res = M('apply_agent')->data($data)->add();
+            if($res){
+                echo "<script>alert('申请成功，请等待客服人员与您联系');location.href='".U('Index/copyPageTwo')."'</script>";
+                exit();
+            }else{
+                echo "<script>alert('申请失败');</script>";
+                echo "<script>javascript:history.back(-1);</script>";die;
+            }
+        }
+    }
+
+    public function user_areaapply(){
+        if(!I('post.')){
+            $this->display();
+        }else{
+            $province = I('post.province');
+            $provinceDataCode = I('post.provinceDataCode');
+            $city = I('post.city');
+            $cityDataCode = I('post.cityDataCode');
+            $area = I('post.area');
+            $area = I('post.areaDataCode');
+            dump(I('post.'));die;
+            if(!$province){
+                echo "<script>alert('请选择要代理的省份');</script>";
+                echo "<script>javascript:history.back(-1);</script>";die;
+            }
+            if(!$provinceDataCode){
+                echo "<script>alert('系统出错，请重新选择要代理的省份');</script>";
+                echo "<script>javascript:history.back(-1);</script>";die;
+            }
+            if(!$city){
+                echo "<script>alert('请选择要代理的城市');</script>";
+                echo "<script>javascript:history.back(-1);</script>";die;
+            }
+            if(!$cityDataCode){
+                echo "<script>alert('系统出错，请重新选择要代理的省份');</script>";
+                echo "<script>javascript:history.back(-1);</script>";die;
+            }
+            $userid = session('userid');
+            $data['uid']=$userid;
+            $data['type']=2;
+            $data['province']=$province;
+            $data['province_code']=$provinceDataCode;
+            $data['city']=$city;
+            $data['city_code']=$cityDataCode;
+            $date['time']=time();
+            $data['status']=0;
+            $res = M('apply_agent')->data($data)->add();
+            if($res){
+                echo "<script>alert('申请成功，请等待客服人员与您联系');location.href='".U('Index/copyPageTwo')."'</script>";
+                exit();
+            }else{
+                echo "<script>alert('申请失败');</script>";
+                echo "<script>javascript:history.back(-1);</script>";die;
+            }
         }
     }
 }
